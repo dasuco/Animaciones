f4d293fa94799dedf941320e7be1b8e46b68b274
DavidSucias <david.sucias@gft.com>
2021-07-14 15:13:04 +0200

    Initial Commit

---
+++ Animaciones/AnimacionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimacionesApp.swift
+//  Animaciones
+//
+//  Created by Sucias Colomer, David on 14/7/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimacionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
